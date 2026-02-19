11797eac559639e521c170a06957cfd55aa94938
Elias <elias.chaghouri@hotmail.com>
2026-02-19 18:37:39 +1100

    Improved functionality

--- main-calendar.js
+++ main-calendar.js
@@ -1,6 +1,8 @@
 const calenderName = localStorage.getItem("calanderName") //gets item from local storage called calendar name
 document.querySelector("h1").textContent = calenderName //sets textContent of h1 to calenderName
 const cells = document.querySelectorAll(".cell")
+const reset = document.getElementById("reset")
+reset.addEventListener("click", resetCalander);
 const saveButton = document.getElementById("saveButton");
 saveButton.addEventListener("click", save)
 cells.forEach(function(cell) {
@@ -20,4 +22,22 @@ function save() {
         localStorage.setItem("cell-" + index, cell.textContent);
     })
     alert("Schedule saved!")
+}
+
+cells.forEach(getSavedItems)
+
+function getSavedItems(cell, index) {
+    const savedContent = localStorage.getItem("cell-" + index)
+    if (savedContent) {
+        cell.textContent = savedContent
+        cell.style.color="#000"
+    }
+}
+
+function resetCalander() {
+    cells.forEach(reset2)
+}
+
+function reset2(cell, index) {
+    cell.textContent = ""
 }
